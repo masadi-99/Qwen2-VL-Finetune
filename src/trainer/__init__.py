51e68b9dc45a8a91f78aaee51e335f450be95356
MohamadAsadi78 <masadi@stanford.edu>
2025-07-31 02:15:45 -0700

    🔧 Fix import error in trainer/__init__.py
    Remove deleted QwenGRPOTextTrainer import

--- src/trainer/__init__.py
+++ src/trainer/__init__.py
@@ -1,12 +1,10 @@
 from .grpo_trainer import QwenGRPOTrainer
-from .grpo_text_trainer import QwenGRPOTextTrainer
 from .dpo_trainer import QwenDPOTrainer
 from .sft_trainer import QwenSFTTrainer
 from .cls_trainer import QwenCLSTrainer
 
 __all__ = [
     "QwenGRPOTrainer",
-    "QwenGRPOTextTrainer", 
     "QwenDPOTrainer",
     "QwenSFTTrainer",
     "QwenCLSTrainer",
